267ad44f36d7b867990db9d19ab86ed3b44fcdd2
vinothrobinson <vinothrobinson03@gmail.com>
2023-03-01 10:33:01 -0500

    Add fix() function to lab6.py

--- lab6.py
+++ lab6.py
@@ -139,12 +139,20 @@ class RBTree:
                 self.__insert(node.right, value)
 
     def fix(self, node):
-        #You may alter code in this method if you wish, it's merely a guide.
         if node.parent is None:
             node.make_black()
+            return
         while node is not None and node.parent is not None and node.parent.is_red():
-            pass
-            #TODO
+            if node.is_right_child():
+                node.rotate_left()
+                if node.parent.parent == None:
+                    node.parent = self.root
+            elif node.is_left_child() and node.left.is_red():
+                node.rotate_right()
+                if node.parent.parent == None:
+                    node.parent = self.root
+            else:
+                node = node.parent
         self.root.make_black()
 
     def __str__(self):
