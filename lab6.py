018c5b70d36ebe320abfae58a226834ef61045b8
Brandon Bosman <brandonb@wightman.ca>
2023-03-03 20:15:15 -0500

    Fixed fix method, and added is_valid method

--- lab6.py
+++ lab6.py
@@ -143,30 +143,33 @@ class RBTree:
         if node.parent is None:
             node.make_black()
             return
-        while node is not None and node.parent is not None: #and (node.parent.is_red() or (node.is_right_child() and node.is_red())):
-            print(self)
-            print(node)
-            if node.is_right_child():
+        while node is not None: #and (node.parent.is_red() or (node.is_right_child() and node.is_red())):
+            print(f"Current node: {node}")
+            print(f"Current root: {self.root}")
+            print(f"Current tree: {self}")
+
+            if node.is_red() and node.parent is not None and node.is_right_child() and (node.parent.left is None or not node.parent.left.is_red()):
+                print("Left rotation")
                 node.rotate_left()
-                # if node.parent == None:
-                #     node.parent = self.root
-            elif node.is_left_child() and node.left is not None and node.is_red() and node.left.is_red():
-                print("we running this")
+                if node.parent is None:
+                    self.root = node
+            elif node.parent is not None and node.is_left_child() and node.left is not None and node.is_red() and node.left.is_red():
+                print("Right rotation")
                 node.rotate_right()
-                self.root = node
-                if node.left.is_red() and node.right.is_red():
-                    node.left.make_black()
-                    node.right.make_black()
-                    if node.is_black():
-                        node.make_red()
-                    else:
-                        node.make_black()
-                # if node.parent == None:
-                #     node.parent = self.root
+                if node.parent is None:
+                    self.root = node
+            elif node.left is not None and node.right is not None and node.left.is_red() and node.right.is_red():
+                print("Colour change")
+                node.left.make_black()
+                node.right.make_black()
+                if node.is_black():
+                    node.make_red()
+                else:
+                    node.make_black()
             else:
                 node = node.parent
-        if node.parent == None:
-            self.root = node
+        # if node.parent == None:
+        #     self.root = node
         self.root.make_black()
 
     def __str__(self):
@@ -183,17 +186,52 @@ class RBTree:
             return "[" + self.__str_helper(node.left) + " <- " + str(node) + "]"
         return "[" + self.__str_helper(node.left) + " <- " + str(node) + " -> " + self.__str_helper(node.right) + "]"
 
+    def find(self, k):
+        current_node = self.root
+        while current_node is not None:
+            if current_node.value < k:
+                current_node = current_node.right
+            elif current_node.value > k:
+                current_node = current_node.left
+            else:
+                return current_node
+        return None
 
+    def black_path_length(self, node):
+        count = 0
+        current_node = node
+        while current_node is not None:
+            if current_node.is_black():
+                count += 1
+            current_node = current_node.parent
+        return count
+
+    def is_valid(self, values):
+        black_length = None
+        for value in values:
+            node = self.find(value)
+            if node.is_leaf():
+                if black_length is None:
+                    black_length = self.black_path_length(node)
+                elif self.black_path_length(node) != black_length:
+                    print("Not valid (number of black lengths not consistent)")
+                    return False
+            if node.parent is not None and node.is_red() and node.parent.is_red():
+                print("Not valid (two red links in a row)")
+                return False
+            if node.is_red() and node.is_right_child():
+                print("Not valid (right-leaning red link)")
+                return False
+        return True
+
+
+values = [1, 50, 26, 54, 99, 80, 56, 19, 51, 35, 14, 15, 73, 23, 76, 43]
 T = RBTree()
-T.insert(10)
-T.insert(5)
-T.insert(2)
-print(T)
-
-# T = RBTree()
-# T.insert(100)
-# T.insert(200)
-# T.insert(10)
-# T.insert(20)
-# print(T)
-# print(T)
+valList = []
+for value in values:
+    T.insert(value)
+    print(T.root)
+    print(T)
+    valList.append(value)
+    if not T.is_valid(valList):
+        break
